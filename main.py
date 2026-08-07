340aba4f2c1a6d7c1c6ab88d09f52c1096ee7ec1
ahmadelectronic <ahmadelectronic@gmail.com>
2026-08-07 17:06:10 +0330

    add roboeq

--- main.py
+++ main.py
@@ -218,6 +218,25 @@ def find_price(soup, text):
             return m.group(1), m.group(2)
     
 
+    # roboeq.ir style
+
+    for span in soup.find_all(
+        "span",
+        class_=lambda c: c and "rial-symbol" in c
+    ):
+
+        price_text = span.get_text(" ", strip=True)
+
+        m = re.search(
+            r'([\d۰-۹,٬.]+)',
+            price_text
+        )
+
+        if m:
+            return m.group(1), "ریال"
+
+
+    
     return None
 
 for index, link in enumerate(df["Link"]):
