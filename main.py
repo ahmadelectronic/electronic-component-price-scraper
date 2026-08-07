5acc873579567042110ad671cac736013c957a5d
ahmadelectronic <ahmadelectronic@gmail.com>
2026-08-07 18:34:11 +0330

    add kavir

--- main.py
+++ main.py
@@ -365,7 +365,25 @@ def find_price(soup, text):
         if m:
             return m.group(1), m.group(2)
 
+    # https://kavirelectronic.ir/ style
 
+    element = soup.select_one(
+        "div.current-price span.price"
+    )
+
+    if element:
+
+        txt = element.get_text(" ", strip=True)
+
+        m = re.search(
+            r'([\d۰-۹,٬.]+)\s*(ریال|تومان)',
+            txt
+        )
+
+        if m:
+            return m.group(1), m.group(2)
+
+        
        
     return None
 
